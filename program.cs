8173f6e7782dda182136b5bc40003a40b0412ec0
Zmo3awed <zyad.mohamed.1577@gmail.com>
2026-08-01 13:24:59 +0300

    Update program.cs

--- program.cs
+++ program.cs
@@ -2,147 +2,50 @@ using System.Text;
 
 namespace tasks
 {
-   
+
     internal class Program
+
     {
-        static void Print(string message)
-        {
+        static void Main(string[] args) { 
+   
 
-            Console.WriteLine(message);
-
-        }
-        static void PrintTitle(string title)
-        {
-            Console.WriteLine($"Book Title : {title}");
-        }
-        static int AddBounce(int pages)
-        {
-            return pages += 50;
-        }
-        static void Welcome() {
-            Console.WriteLine("Welcome to the Library!");
-        }
-        static double ApplyDiscount(double[] prices)
-        {
-            return prices[0] -= 5;
-        }
-        static int AddBounceByRef( ref int pages)
-        {
-            return pages += 50;
-        }
-        static void ReplaceArray(ref double[] prices)
-        {
-             prices =new  double[] { 10.0, 12.5, 15.0 };
-            
-            
-        }
-        static bool TryGetPrice(string title, out double price)
-        {
-            if(title == null || title != "Clean Code")
+        //Q1
+        //We can not access it because it is private and no one can see this variable outside the class
+        //Q2
+         book book = new book();
+         Console.WriteLine(book.copiesInStock);
+            //compiled normly because internall allow to access at the same project 
+        //Q3
+         book.title = "clean code";
+         Console.WriteLine(book.title);
+        //Q4
+         book.genre = Genre.Science;
+         Console.WriteLine(book.genre);
+        //Q5
+         int genreNumber = 1;
+         book.genre =(Genre) genreNumber;
+        //Q6
+         Genre genre = Genre.Fiction;
+         Console.WriteLine(genre.ToString());
+        //Q7
+         string genreText = "Science";
+         Genre genre2 =(Genre) Enum.Parse(typeof(Genre),genreText);
+         Console.WriteLine(genre2);
+         //Q8
+         string genreText2 = "Mystery";
+         bool isparsed = Enum.TryParse(genreText2, true,out Genre genre3);
+            if (isparsed)
             {
-                price = 0;
-                return false;
-            }
-            price = 25.5;
-            return true;
-                
-
-
-
-        }
-        static void PrintInfo(string title, int pages = 300)
-        {
-            Console.WriteLine($"Book Title :  {title} numper of pages : {pages}");
-        }
-        static void PrintAllTitles(params string[] titles)
-        {
-            foreach (string title in titles) {
-
-                Console.WriteLine(title);
+                Console.WriteLine("Parsed sucsess");
 
             }
-        }
+            else
+            {
+                Console.WriteLine("Unknown genre");
+            }
+         
 
-        static void Main(string[] args)
-                {
-           
-            ////Q1
-            //double[] prices = { 25.5,40,45.25};
-            //Console.WriteLine(prices[1]);
-            ///***********************************************************/
-            ////Q2
-            //int[,] selfCopies = {
 
-            //    {3,5 },
-            //    {1,4 }
-
-            //     };
-            //Console.WriteLine(selfCopies[1,0]);
-            /************************************************************/
-            //Q3 
-            //Welcome();
-
-            //Q4
-
-            //PrintTitle("Clean Code");
-            /******************************************************/
-            //Q5
-
-            //int pages = 400;
-
-            //Console.WriteLine($"pages Befor call : {pages}");
-            //AddBounce(pages); // I expect to see the same value because it is value type and i passed prameter by value 
-            //Console.WriteLine($"pages After call : {pages}");
-            /*******************************************************************************************/
-            //Q6
-            //double[] prices = { 25.5, 40.0 };
-            //Console.WriteLine($"prices befor : {prices[0]}");
-            //ApplyDiscount(prices);
-            //Console.WriteLine($"prices after : {prices[0]}");
-            /****************************************************************************************/
-            //Q7 
-
-            //int pages = 400;
-
-            //Console.WriteLine($"pages Befor call : {pages}");
-            //AddBounceByRef(ref pages); // I expect to see deffrent value because we paased by ref so the variable it self will be passed 
-            //Console.WriteLine($"pages After call : {pages}");
-            /*****************************************************************************************/
-            //Q8 
-            //double[] prices = { 25.5, 40.0 };
-            //Console.WriteLine($"Arr befor calling : ");
-            //foreach (double pr in prices) { 
-            //Console.Write($"{pr} ");
-
-            //}
-            //ReplaceArray(ref prices);
-            //Console.WriteLine($"\nArr after calling : ");
-            //foreach (double pr in prices)
-            //{
-            //    Console.Write($"{pr} ");
-
-            //}
-            /****************************************************************************************/
-            //Q9
-            //bool thereprice = TryGetPrice("Clean Code", out double price);
-            //if (thereprice)
-            //{
-            //    Console.WriteLine($"Price is : {price}");
-
-            //}
-            //else
-            //{
-            //    Console.WriteLine("There is no price");
-            //}
-            /******************************************************************************************/
-            //Q10/11
-            //PrintInfo("Clean Code ");
-            //PrintInfo("Clean Code ", 400);
-            //PrintInfo(pages:400,title:"Clean Code");
-            /******************************************************************************************/
-            //Q12
-            //PrintAllTitles("Clean Code", "The Pragmatic Programmer", "Design Patterns");
-           
 
 
 
@@ -153,3 +56,5 @@ namespace tasks
         }
     }
 }
+   
+    
